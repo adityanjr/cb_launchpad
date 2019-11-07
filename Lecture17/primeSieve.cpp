06280081f56fff79d5b5701049aa07116ec128e7
Aditya Singh <samant04aditya@gmail.com>
2019-11-07 12:56:47 +0530

    ...

--- Lecture17/primeSieve.cpp
+++ Lecture17/primeSieve.cpp
@@ -32,25 +32,25 @@ int main(int argc, char const *argv[])
 	}
 	cout<<endl;
 
-// Optimization 1
-	arr[2] =1;
-	// initially mark all odd numbers as Prime
-	for (int i = 3; i < n; ++i)
-		{	if(arr[i]&1){
-				arr[i] =1;
-			}
-		}
-		//check multiples of odd numbers only
-		for (int i = 3; i < n ; i+=2)
-		{
-			if(arr[i] != 0){
-				// start from i*i as non prime numbers(multiples of i) smaller than i*i are already marked as non-prime by other smaller numbers
-				for (int j = i*i; j < n ; j+=i)
-				{
-					arr[j] =0;
-				}
-			}
-		}
+// // Optimization 1
+// 	arr[2] =1;
+// 	// initially mark all odd numbers as Prime
+// 	for (int i = 3; i < n; ++i)
+// 		{	if(arr[i]&1){
+// 				arr[i] =1;
+// 			}
+// 		}
+// 		//check multiples of odd numbers only
+// 		for (int i = 3; i < n ; i+=2)
+// 		{
+// 			if(arr[i] != 0){
+// 				// start from i*i as non prime numbers(multiples of i) smaller than i*i are already marked as non-prime by other smaller numbers
+// 				for (int j = i*i; j < n ; j+=i)
+// 				{
+// 					arr[j] =0;
+// 				}
+// 			}
+// 		}
 
 // Optimization 2
 	arr[2] =1;
